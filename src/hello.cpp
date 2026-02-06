343c5718430c4fb0e1a9f59a188745fbbd75e823
傅冠智 <gzfu422@gmail.com>
2026-02-06 21:29:07 +0800

    Enhance hello.cpp with PID output and instructions
    Added output for process ID and instructions to user.

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,9 +1,14 @@
 #include<bits/stdc++.h>
+#include<iostream>
+#include<unistd.h> // for get pid 
 
 using namespace std;
 
 int main()
 {
     cout<<"hello, word"<<endl;
+    cout<<"My pid"<<getpid()<<endl;     //pid is the unique number for each program
+    cout<<"Now stop . You can get pid and see the mapview ."<<endl;
+    cin.get();  //pause the program 
     return 0;
 }
