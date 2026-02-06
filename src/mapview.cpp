cba92710a5adc8f571703da57faa2263448eb9fd
傅冠智 <gzfu422@gmail.com>
2026-02-06 21:34:01 +0800

    Refactor mapview.cpp for clarity and structure

--- src/mapview.cpp
+++ src/mapview.cpp
@@ -1,49 +1,101 @@
+
 #include<iostream>
-#include<fstream>  //file stream
+#include<fstream>   //file stream
 #include<string>
-#include<cstdlib>   // provide exit to stop program and see the mapview
-#include<cerrno>    //show what happend when open the file " errno "
-#include<cstring>   //use strerror "strerror"
+#include<cstdlib>   //exit
+#include<cerrno>    //"errno" show the error
+#include<cstring>   //"strerror" translate the error 
+#include<unistd.h>  //fork , execv , pid_t
+#include<sys/ptrace.h>  //ptrace , PTRACE_TRACEME , ptrace_CONT , PTRACE_KILL
+#include<sys/wait.h>    //waitpid , WIFISTOPPED
 
-using namespace std;
-
-int main(int argc , char*argv[])
+void print_how_to_use(const char* program_name)
 {
-    if(argc != 2){
-        // cerr : the different way for direct output
-        cerr<<"error "<<argv[0]<<" need the target ID"<<endl;
-        cerr<<"For example "<<argv[0]<<" 1234";
-        exit(1);    //return error 1
+    using namespace std;
+    cerr<<"Error "<<program_name<<" need the target path"<<endl;
+    cerr<<"For example : "<<program_name<<" ./hello"<<endl;
+    return ;
+}
+
+pid_t start_child_and_father_program(const char*target_path)  //pid_t = int
+{
+    using namespace std;
+    pid_t pid = fork(); //it will make two program 
+                        //one is the child program it pid is 0
+                        //the other is the father program it pid is not 0
+    
+    if(pid == -1){  //pid = -1 means error
+        cerr<<"Fork error"<<strerror(errno)<<endl;
+        return -1;
     }
 
-    
-    string id = argv[1] , path = "/proc/" + id + "/maps";
-    // the "path" store the detail about the target project
+    else if(pid == 0){      //child program
+        ptrace(PTRACE_TRACEME , 0 , nullptr , nullptr);   // "PTRACE_TRACEME" let the father program trace
+        char*args[] = {const_cast<char*>(target_path) , nullptr};  //const_cast change "const char" to "char" tempory
+                                                                   //args[0] = target path , args[1] = nullptr means the args end 
+        execv(target_path , args);  //jump to target program   "args" will be placed in the main(args[0]) 
 
-    cout<<"reading : "<<path<<endl;
-    cout<<"========================="<<endl;
-    cout<<endl;
-
-    // "c_str" change string to char
-    ifstream file(path.c_str());    //open file 
-    
-    if(!file){  //!file means canʼt opem the file
-        cerr<<"Error : "<<strerror(errno)<<" pid = "<<id<<endl;
+        //if failed the following will run
+        cerr<<"Errno : "<<strerror(errno)<<endl;
         exit(1);
     }
 
-    cout<<"Memory Map Mapping"<<endl;
-    cout<<endl;
-    string mapping;
-
-    while(getline(file , mapping)){
-        cout<<mapping<<endl;
+    else{   //itʼs father program pid is the child id
+        int status;
+        waitpid(pid , &status , 0); //waitpid will wait until the child program pause
+                                    //"0" means the function will return when the child program pause or end
+                                    // & will convey the location so the function can modift the value
+        if(WIFSTOPPED(status)){
+                return pid; //child pid
+        }
+        else{
+            return -1;
+        }
     }
+}
 
-    file.close();   //close the file
+void print_memory_maps(pid_t pid)
+{
+    using namespace std;
+    string path = "/proc/" + to_string(pid) + "/maps";  //file path
+    ifstream file(path);    //open file
 
-    cout<<"End"<<endl;
-    cout<<"========================="<<endl;
+    if(!file){
+        cerr<<"Error : "<<strerror(errno)<<" "<<path<<endl;
+        return ;
+    }
+    string line;
+    while(getline(file , line)){
+        cout<<line<<endl;
+    }
+    file.close();
+}
 
+void clean_child_program(pid_t child_pid)
+{
+    using namespace std;
+    ptrace(PTRACE_KILL ,child_pid , nullptr , nullptr);
+    //kill the child program
+    int status;
+    waitpid(child_pid , &status , 0);
+    cout<<"child_program_end"<<endl;
+    return ;
+}
+
+int main(int argc , char*argv[])
+{
+    using namespace std;
+    if(argc != 2){
+        print_how_to_use(argv[0]);  //argv[0] = program name
+        exit(1);
+    }
+    const char* target_path = argv[1];
+    pid_t child_pid = start_child_and_father_program(target_path);
+    if(child_pid == -1){
+        cerr<<"Error"<<endl;
+        exit(1);
+    }
+    print_memory_maps(child_pid);
+    clean_child_program(child_pid);
     return 0;
 }
