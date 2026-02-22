c015f47ca831123b441b80b77d8136ddfa498ede
傅冠智 <gzfu422@gmail.com>
2026-02-22 22:14:04 +0800

    Add function declarations for process management

--- src/process/process.h
+++ src/process/process.h
@@ -1,1 +1,7 @@
+#pragma once
 
+#include<sys/types.h>   //data ensure 
+
+void print_how_to_use(const char* program_name);
+pid_t start_child_and_father_program(const char*target_path);  //pid_t = int
+void clean_child_program(pid_t child_pid);
