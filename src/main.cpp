45ee316e71b4515c919ff3584ff4704963a6e707
傅冠智 <gzfu422@gmail.com>
2026-02-26 13:08:36 +0800

    Use ELF to see the function address(experimental)
    Add ELF function in main.cpp
    
    Add symbols.cpp to the build configuration
    
    Integrate symbol handling in main program
    
    Added symbol reading and address printing functionality.
    
    Add check_is_pie function declaration
    
    Fix error message and add check_is_pie function
    
    Refactor MemoryRegion structure and update enum class
    
    Fix formatting and improve code readability in memory.cpp
    
    Implement symbol reading and printing functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
-#include<iostream>
-#include<cstdlib>
-#include<vector>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
 
-#include"process/process.h"
-#include"memory/memory.h"
-#include"common/color.h"
+#include "process/process.h"
+#include "memory/memory.h"
+#include "common/color.h"
+#include "elf/elf_view.h"
+#include "symbols/symbols.h"
 
 using namespace std;
 
-int main(int argc , char*argv[])
+int main(int argc, char *argv[])
 {
     using namespace std;
-    if(argc != 2){
-        print_how_to_use(argv[0]);  //argv[0] = program name
+    if (argc != 2)
+    {
+        print_how_to_use(argv[0]); // argv[0] = program name
         exit(1);
     }
-    const char* target_path = argv[1];
+    const char *target_path = argv[1];
+
+    if (!print_elf_info(target_path))
+    {
+        cerr << "[main]: Target is not a valid ELF";
+        return -1;
+    }
+
+    vector<Symbol> symbols = read_symbols(target_path);
+
+    if (symbols.empty())
+    {
+        cerr << "No symbol found" << endl;
+        return -1;
+    }
+
     pid_t child_pid = start_child_and_father_program(target_path);
-    if(child_pid == -1){
-        cerr<<"Error"<<endl;
+    if (child_pid == -1)
+    {
+        cerr << "Error" << endl;
         exit(1);
     }
 
@@ -26,7 +45,12 @@ int main(int argc , char*argv[])
 
     print_colored_maps(regions);
 
+    uint64_t base_address = get_base_address(regions, target_path);
+
+    bool is_pie = check_is_pie(target_path);
+
+    print_symbol_addresses(symbols, base_address, is_pie);
+
     clean_child_program(child_pid);
     return 0;
 }
-
