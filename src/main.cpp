5d7c7d9ed7d4cfcd65a51389405ecc9a8536a1cd
傅冠智 <gzfu422@gmail.com>
2026-02-26 13:08:36 +0800

    Add ELF function in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,34 @@
-#include<iostream>
-#include<cstdlib>
-#include<vector>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
 
-#include"process/process.h"
-#include"memory/memory.h"
-#include"common/color.h"
+#include "process/process.h"
+#include "memory/memory.h"
+#include "common/color.h"
+#include "elf/elf_view.h"
 
 using namespace std;
 
-int main(int argc , char*argv[])
+int main(int argc, char *argv[])
 {
     using namespace std;
-    if(argc != 2){
-        print_how_to_use(argv[0]);  //argv[0] = program name
+    if (argc != 2)
+    {
+        print_how_to_use(argv[0]); // argv[0] = program name
         exit(1);
     }
-    const char* target_path = argv[1];
+    const char *target_path = argv[1];
+
+    if (!print_elf_info(target_path))
+    {
+        cerr << "[main]: Target is not a valid ELF";
+        return -1;
+    }
+
     pid_t child_pid = start_child_and_father_program(target_path);
-    if(child_pid == -1){
-        cerr<<"Error"<<endl;
+    if (child_pid == -1)
+    {
+        cerr << "Error" << endl;
         exit(1);
     }
 
@@ -29,4 +39,3 @@ int main(int argc , char*argv[])
     clean_child_program(child_pid);
     return 0;
 }
-
