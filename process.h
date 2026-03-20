104fa260ff76a6d5c764c55f1a149c329f8352fe
傅冠智 <gzfu422@gmail.com>
2026-03-20 13:01:35 +0800

    Delete process.h

--- process.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include<sys/types.h>   //data ensure 
-
-void print_how_to_use(const char* program_name);
-pid_t start_child_and_father_program(const char*target_path);  //pid_t = int
-void clean_child_program(pid_t child_pid);
